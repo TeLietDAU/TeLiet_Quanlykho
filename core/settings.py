4a3c21afff8c6895a9ff9c180e29953ea5fa58c5
tin14204 <tin14204@gmail.com>
2026-03-15 19:06:07 +0700

    Revert "Thêm middleware, view và url mẫu cho product"
    This reverts commit 2ca0c0c9fb38c4c8c852034fc3f8c190e5d6fb03.

--- core/settings.py
+++ core/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middlewares.xu_ly_loi.XuLyLoiMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
