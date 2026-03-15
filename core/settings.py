2ca0c0c9fb38c4c8c852034fc3f8c190e5d6fb03
tin14204 <tin14204@gmail.com>
2026-03-15 18:39:39 +0700

    Thêm middleware, view và url mẫu cho product

--- core/settings.py
+++ core/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middlewares.xu_ly_loi.XuLyLoiMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
